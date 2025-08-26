7d084bdf8706c87455040e48c92a33893c9031e7
Aayush Singh <140009530+Aayushsingh2001@users.noreply.github.com>
2025-08-27 00:18:08 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://codewithaayu:codewithaayu@fooddelivery.khmagic.mongodb.net/?retryWrites=true&w=majority&appName=FoodDelivery").then(()=>console.log("DB Connected"));
-
-}
-export default connectDB;
