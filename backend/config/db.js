ccbe5c418e55869b2ac91f11085f4f4bb23647af
Aayushsingh2001 <as6994770@gmail.com>
2025-09-16 14:27:57 +0530

    Added backend secrets

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const connectDB = async ()=>{
-    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
+    // await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
+    await mongoose.connect("${{ secrets.MONGO_URL }}").then(()=>console.log("DB Connected"));
 
 }
 export default connectDB;
